4daa0563cc319a4df73fb9baf31fee78c2856d42
Scott VanRavenswaay <scottvr@gmail.com>
2024-06-06 17:48:02 -0500

    Update EtchABot.cpp
    Add missing pow() in radius calculation. Change < to <= so we don’t miss that last seg.

--- EtchABot.cpp
+++ EtchABot.cpp
@@ -217,7 +217,7 @@ void EtchABot::drawArc(int xCenter, int yCenter, int nSegs, float degrees) {
 	int startX = getX();
 	int startY = getY();
 	
-	float radius = sqrt((startX - xCenter, 2) + pow(startY - yCenter, 2));
+	float radius = sqrt(pow(startX - xCenter, 2) + pow(startY - yCenter, 2));
 	float radiansPerSeg = degToRad*degrees/nSegs;
 	
 	// Store the existing backlash values, so we can reset them at the end
@@ -227,10 +227,10 @@ void EtchABot::drawArc(int xCenter, int yCenter, int nSegs, float degrees) {
 	setHBacklash(0);
 	setVBacklash(0);
 	float xNext, yNext;
-	for (int i = 1; i < nSegs; i++) {
+	for (int i = 1; i <= nSegs; i++) {
 		xNext = startX + radius*cos(i*radiansPerSeg);
 		yNext = startY - radius*sin(i*radiansPerSeg);  // subtract b/c positive y is down
-		drawLine(xNext, yNext);
+		drawLine(static_cast<int>xNext, static_cast<int>yNext, false);
 	}
 	// Restore previous backlash settings
 	setHBacklash(hBacklash);
